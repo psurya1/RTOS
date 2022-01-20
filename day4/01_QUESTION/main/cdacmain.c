617911c37311776f3770933e128322c2c33f2283
psurya1 <suryaswaminathan1999@gmail.com>
2022-01-20 18:07:43 +0530

    Task in RTOS

--- day4/01_QUESTION/main/cdacmain.c
+++ day4/01_QUESTION/main/cdacmain.c
@@ -2,38 +2,42 @@
 #include <freertos/FreeRTOS.h>
 #include <freertos/task.h>
 
-TaskHandle_t xHandleTaskLog_1,xHandleTaskLog_2;
+TaskHandle_t xHandleTaskLog_1;
+TaskHandle_t xHandleTaskLog_2;
 
-void *task_1(void *pvparameters)
+void task_1(void *pvparameters)
 {
     while(1)
     {
         printf("TASK 1 RUNNING\n");
+       // vTaskDelay(1000/ portTICK_PERIOD_MS);
     }
     vTaskDelete(NULL);
 }
-void *task_2(void *pvparameters)
+void task_2(void *pvparameters)
 {
     while(1)
     {
         printf("TASK 2 RUNNING\n");
+       // vTaskDelay(1000/ portTICK_PERIOD_MS);
     }
     vTaskDelete(NULL);
 }
 void app_main()
 {
     BaseType_t result;
-    result=xTaskCreate(task_1,"task_1",2048,NULL,5,&xHandleTaskLog_1);
+    
+    result=xTaskCreate(task_1,"task_1",1024,NULL,5,&xHandleTaskLog_1);
 
     if(result==pdPASS)
     {
         printf("task_1 created\n");
     }
-
-    result=xTaskCreate(task_2,"task_2",2048,NULL,4,&xHandleTaskLog_2);
+    result=xTaskCreate(task_2,"task_2",1024,NULL,3,&xHandleTaskLog_2);
 
     if(result==pdPASS)
     {
         printf("task_2 created\n");
     }
+    
 }

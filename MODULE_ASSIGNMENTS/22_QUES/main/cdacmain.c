1472e33df2ab063b994b4b6324be04218b66af9e
psurya1 <suryaswaminathan1999@gmail.com>
2022-01-26 22:29:41 +0530

    RTOS MODULE ASSIGNMENT

--- MODULE_ASSIGNMENTS/22_QUES/main/cdacmain.c
+++ MODULE_ASSIGNMENTS/22_QUES/main/cdacmain.c
@@ -4,23 +4,19 @@
 #include <driver/gpio.h>
 #include <freertos/queue.h>
 
+#define BUTTON 2
 //usage of queues within an interrupt service routine
 
 QueueHandle_t queue;
 TaskHandle_t xHandle_1;
 TaskHandle_t xHandle_2;
-
+uint32_t button_val;
 
 void sensortask_1(void *pvparameters)
 {
-    int sensor_data=0;
-    while(1)
-    {
-        sensor_data++;
-        printf("SENSOR TASK  RUNNING: STACK SIZE %d\n",uxTaskGetStackHighWaterMark(NULL));
-        xQueueSendFromISR(queue,&sensor_data,NULL);
-        vTaskDelay(1000/ portTICK_PERIOD_MS);
-    }
+    button_val=gpio_get_level(BUTTON);
+    //printf("BUTTON PRESSED :%d\n",button_val);
+    xQueueSendFromISR(queue,&button_val,NULL);
 }
 void Alarmtask_1(void *pvparameters)
 {
@@ -38,12 +34,17 @@ void app_main()
     queue=xQueueCreate(10,sizeof(int));
     BaseType_t result;
     
-    result=xTaskCreate(sensortask_1,"sensortask_1",2048,NULL,5,&xHandle_1);
+    //result=xTaskCreate(sensortask_1,"sensortask_1",2048,NULL,5,&xHandle_1);
 
-    if(result==pdPASS)
-    {
-        printf("sensortask created\n");
-    }
+    
+    gpio_reset_pin(BUTTON);
+    gpio_set_direction(BUTTON,GPIO_MODE_INPUT);
+
+    gpio_intr_enable(BUTTON);
+    gpio_set_intr_type(BUTTON,GPIO_INTR_ANYEDGE);
+    gpio_install_isr_service(ESP_INTR_FLAG_EDGE);
+    gpio_isr_handler_add(BUTTON,sensortask_1,0);
+
     result=xTaskCreate(Alarmtask_1,"Alarmtask_1",2048,NULL,5,&xHandle_2);
 
     if(result==pdPASS)
